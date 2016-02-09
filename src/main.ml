af4e10cf434c955987ec883a37ba226bd58e71ba
Kirill Mokevnin <mokevnin@gmail.com>
2016-02-10 01:55:19 +0300

    add fetch command

--- src/main.ml
+++ src/main.ml
@@ -1,22 +1,30 @@
 let hexlet_dir = Filename.concat (Unix.getenv "HOME") "hexlet-challenges";;
+let course_name = ref "";;
 
-let init =
+let init () =
   if not (Sys.file_exists hexlet_dir) then begin
     Printf.printf "'%s' has been created\n" hexlet_dir;
     Unix.mkdir hexlet_dir 0o755
   end else
-    Printf.printf "'%s' already exists\n" hexlet_dir
+    Printf.printf "'%s' already exists\n" hexlet_dir;
+  ()
+;;
+
+let fetch () =
+  Sys.command (Printf.sprintf "git clone https://github.com/hexlet-challenges/course-%s.git" !course_name);
+  ()
 ;;
 
 (*raise (Failure argument)*)
 let action argument = match argument with
-  "init" -> init
+  | "init" -> init ()
+  | "fetch" -> fetch ()
   | _ -> failwith "Unknown command"
 ;;
 
 let usage = "usage: " ^ Sys.argv.(0) ^ " <course's slug>"
 let speclist = [
-  (*"init", Arg.Symbol (init), ": some int parameter"*)
+  "-c", Arg.Set_string (course_name), ": some int parameter"
 ]
 
 let () =
