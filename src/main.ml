f34cbf98000911b7af2a078b2b9855b58a73564b
Kirill Mokevnin <mokevnin@gmail.com>
2016-02-10 00:38:55 +0300

    add init

--- src/main.ml
+++ src/main.ml
@@ -1,2 +1,7 @@
-print_string "Hello world!\n";;
+let hexlet_dir = Filename.concat (Unix.getenv "HOME") "hexlet-challenges";;
 
+let init =
+  if not (Sys.file_exists hexlet_dir) then
+    Unix.mkdir hexlet_dir 0o755;;
+
+init
