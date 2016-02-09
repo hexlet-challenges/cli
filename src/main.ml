afb2cb6dab448ca3d47d7bb77fd236985e635a97
Kirill Mokevnin <mokevnin@gmail.com>
2016-02-10 01:31:53 +0300

    parse args

--- src/main.ml
+++ src/main.ml
@@ -1,7 +1,28 @@
 let hexlet_dir = Filename.concat (Unix.getenv "HOME") "hexlet-challenges";;
 
 let init =
-  if not (Sys.file_exists hexlet_dir) then
-    Unix.mkdir hexlet_dir 0o755;;
+  if not (Sys.file_exists hexlet_dir) then begin
+    Printf.printf "'%s' has been created\n" hexlet_dir;
+    Unix.mkdir hexlet_dir 0o755
+  end else
+    Printf.printf "'%s' already exists\n" hexlet_dir
+;;
 
-init
+(*raise (Failure argument)*)
+let action argument = match argument with
+  "init" -> init
+  | _ -> failwith "Unknown command"
+;;
+
+let usage = "usage: " ^ Sys.argv.(0) ^ " <course's slug>"
+let speclist = [
+  (*"init", Arg.Symbol (init), ": some int parameter"*)
+]
+
+let () =
+  (* Read the arguments *)
+  Arg.parse
+    speclist
+    (fun x -> (action x))
+    usage;
+;;
